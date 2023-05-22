0393b28c034e7377d20553879bcd4501185b8531
Maik Fröbe <maik.froebe@informatik.uni-halle.de>
2023-05-22 18:00:45 +0200

    Add qrels file

--- milestone-01/ir_anthology.py
+++ milestone-01/ir_anthology.py
@@ -1,5 +1,5 @@
 import ir_datasets
-from ir_datasets.formats import JsonlDocs, TrecXmlQueries
+from ir_datasets.formats import JsonlDocs, TrecXmlQueries, TrecQrels
 from typing import NamedTuple
 from ir_datasets.datasets.base import Dataset
 
@@ -16,5 +16,6 @@ class IrAnthologyDocument(NamedTuple):
 
 ir_datasets.registry.register('iranthology-tutors', Dataset(
     JsonlDocs(ir_datasets.util.PackageDataFile(path='datasets_in_progress/ir-anthology-processed.jsonl'), doc_cls=IrAnthologyDocument, lang='en'),
-    TrecXmlQueries(ir_datasets.util.PackageDataFile(path='datasets_in_progress/topics.xml'), lang='en')
+    TrecXmlQueries(ir_datasets.util.PackageDataFile(path='datasets_in_progress/topics.xml'), lang='en'),
+    TrecQrels(ir_datasets.util.PackageDataFile(path='datasets_in_progress/qrels.txt'), {0: 'Not Relevant', 1: 'Relevant'})
 ))
